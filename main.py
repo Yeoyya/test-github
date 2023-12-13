4635a0a948cd3c2b6cf103ce037b774c3bb97360
Yeoyya <118322190+Yeoyya@users.noreply.github.com>
2023-12-13 10:10:25 +0800

    SIal

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 print("Hello World")
+
+def add(x, y):
+    return x, y
+
+print(add(1,1))
+
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
